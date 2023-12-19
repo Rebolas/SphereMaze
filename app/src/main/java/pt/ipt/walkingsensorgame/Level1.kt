b47cbb490285ad5a9568cba9e84ad7ad965b4aea
pzmq <rs_monge@hotmail.com>
2023-12-19 15:04:54 +0000

    Project (General):     - missing references to old project name.     - Added new activities to AndroidManifest.
    MainActivity:
        - Button added to be able to play (with logic).
    
    Level 1 initiated:
        - Diferent Resolution Background and characters.
        - Starting blockouts for walls (visible only in editor).
        - Internal logic using accelerometer with baseline, sensitivity, smoothness features.
        - Temporary TextView for reviewing baselines and current accelerometer values.

--- app/src/main/java/pt/ipt/walkingsensorgame/Level1.kt
+++ app/src/main/java/pt/ipt/walkingsensorgame/Level1.kt
@@ -1,12 +1,150 @@
 package pt.ipt.walkingsensorgame
 
+import android.graphics.Point
+import android.graphics.Rect
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import pt.ipt.WalkingSensorGame.R
 
-class Level1 : AppCompatActivity() {
+//Para duvidas verificar no video https://www.youtube.com/watch?v=xcsuDDQHrLo&ab_channel=Indently
+
+
+class Level1 : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var character: ImageView
+    private lateinit var texto: TextView
+    private lateinit var walls: List<ImageView>
+    private var screenWidth: Float = 0F
+    private var screenHeight: Float = 0F
+    private val smoothness = 0.2f
+    private val sensitivity = 1.5f
+
+    // Baseline variables
+    private var baselineSides: Float = 0f
+    private var baselineUpdown: Float = 0f
+
+    // Flag to track whether it's the first time
+    private var isFirstTime = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_level1)
+
+        character = findViewById(R.id.character)
+        texto = findViewById(R.id.ACELAROMETRO)
+
+        // Populate the list of walls
+        walls = listOf(findViewById(R.id.wall1), findViewById(R.id.wall2))
+        // Add more walls as needed
+
+        // Get screen dimensions
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+
+        screenWidth = size.x.toFloat()
+        screenHeight = size.y.toFloat()
+
+        // Set the initial position of the character
+        character.x = screenWidth / 2
+        character.y = screenHeight * (2 / 3)
+
+        // Initialize sensors
+        setupSensors()
+    }
+
+    private fun setupSensors() {
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            val sides = -event.values[0]
+            val updown = event.values[1]
+
+            if (isFirstTime) {
+                // Capture baseline values on the first sensor change event
+                captureBaseline(updown, sides)
+                isFirstTime = false
+            }
+
+            val adjustedSides = (sides - baselineSides) * sensitivity
+            val adjustedUpdown = (updown - baselineUpdown) * sensitivity
+
+            val targetX = character.translationX + adjustedSides
+            val targetY = character.translationY + adjustedUpdown
+
+            // Check for collisions with walls
+            if (isCollidingWithWall(targetX, targetY)) {
+                // Collision detected, adjust target position to prevent crossing any wall
+                character.translationX = character.translationX.coerceIn(0f, screenWidth - character.width)
+                character.translationY = character.translationY.coerceIn(0f, screenHeight - character.height)
+                return
+            }
+
+            // If no collision, update the character's position with smoothness
+            character.translationX = lerp(character.translationX, targetX, smoothness)
+            character.translationY = lerp(character.translationY, targetY, smoothness)
+
+            // Limit character position to the screen boundaries
+            character.translationX = character.translationX.coerceIn(0f, screenWidth - character.width)
+            character.translationY = character.translationY.coerceIn(0f, screenHeight - character.height)
+
+            texto.text = "up/down ${adjustedUpdown.toInt()}\nleft/right ${adjustedSides.toInt()}"
+        }
+    }
+
+    private fun captureBaseline(updown: Float, sides: Float) {
+        baselineSides = sides
+        baselineUpdown = updown
+    }
+
+    private fun isCollidingWithWall(targetX: Float, targetY: Float): Boolean {
+        for (wall in walls) {
+            val wallRect = Rect()
+            wall.getHitRect(wallRect)
+
+            val characterRect = Rect(
+                targetX.toInt(),
+                targetY.toInt(),
+                (targetX + character.width).toInt(),
+                (targetY + character.height).toInt()
+            )
+
+            if (wallRect.intersect(characterRect)) {
+                return true // Collision detected with any wall
+            }
+        }
+
+        return false // No collision with any wall
+    }
+
+    private fun lerp(start: Float, stop: Float, amount: Float): Float {
+        return start + amount * (stop - start)
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        // Not necessary to implement
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
     }
 }
--- app/src/main/java/pt/ipt/walkingsensorgame/MainActivity.kt
+++ app/src/main/java/pt/ipt/walkingsensorgame/MainActivity.kt
@@ -1,12 +1,20 @@
 package pt.ipt.walkingsensorgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import pt.ipt.WalkingSensorGame.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonclick = findViewById<Button>(R.id.Playbutton)
+        buttonclick.setOnClickListener{
+            val intent= Intent(this,Level1::class.java)
+            startActivity(intent)
+        }
     }
 }
