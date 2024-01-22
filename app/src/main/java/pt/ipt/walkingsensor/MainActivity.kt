a60ec7aab0551fdd3ef7a4154b84d942c88b2f04
Rebolas <60193826+Rebolas@users.noreply.github.com>
2024-01-22 00:01:11 +0000

    Ligações entre paginas de score, dados pessoais, e main activity. Atualização da pagina de dados pessoais e scores

--- app/src/main/java/pt/ipt/walkingsensor/MainActivity.kt
+++ app/src/main/java/pt/ipt/walkingsensor/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import pt.ipt.WalkingSensorGame.R
 import pt.ipt.walkingsensor.levels.Level1
 
@@ -16,12 +17,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonclick = findViewById<Button>(R.id.Playbutton)
-        buttonclick.setOnClickListener{
-            val intent= Intent(this, Level1::class.java)
+        val buttonperfil = findViewById<ImageButton>(R.id.imageButtonPerfil)
+        buttonperfil.setOnClickListener {
+            val intent = Intent(this, PersonalDataActivity::class.java)
             startActivity(intent)
+        }
 
-            //overridePendingTransition(R.drawable.fade_in, R.drawable.fade_out);
+        val buttonscoremain = findViewById<Button>(R.id.buttonScores)
+        buttonscoremain.setOnClickListener {
+            val intent = Intent(this, ScoreActivity::class.java)
+            startActivity(intent)
+        }
+
+            val buttonclick = findViewById<Button>(R.id.Playbutton)
+            buttonclick.setOnClickListener {
+                val intent = Intent(this, Level1::class.java)
+                startActivity(intent)
+
+                //overridePendingTransition(R.drawable.fade_in, R.drawable.fade_out);
+            }
         }
     }
-}
--- app/src/main/java/pt/ipt/walkingsensor/PersonalDataActivity.kt
+++ app/src/main/java/pt/ipt/walkingsensor/PersonalDataActivity.kt
@@ -1,12 +1,21 @@
 package pt.ipt.walkingsensor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import pt.ipt.WalkingSensorGame.R
 
 class PersonalDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
+
+        val buttonvoltardados = findViewById<Button>(R.id.ButtonVoltarSobre)
+        buttonvoltardados.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
