d39eb3df8c990a26f4195001268461769ac0428b
Rebolas <60193826+Rebolas@users.noreply.github.com>
2024-01-23 00:52:45 +0000

    Atualização da pagina de scores e da pagina de dados pessoais

--- app/src/main/java/pt/ipt/walkingsensor/PersonalDataActivity.kt
+++ app/src/main/java/pt/ipt/walkingsensor/PersonalDataActivity.kt
@@ -1,10 +1,15 @@
 package pt.ipt.walkingsensor
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
+import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.view.isVisible
 import pt.ipt.WalkingSensorGame.R
 
 class PersonalDataActivity : AppCompatActivity() {
@@ -16,6 +21,29 @@ class PersonalDataActivity : AppCompatActivity() {
         buttonvoltardados.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+        val nameTextView = findViewById<TextView>(R.id.textViewNome)
+        val textViewEmail = findViewById<TextView>(R.id.textViewEmail)
+        val nameEdit = findViewById<EditText>(R.id.editUsernamePerfil)
+        val emailEdit = findViewById<EditText>(R.id.editEmailPerfil)
+
+        val buttonEditarDados=findViewById<Button>(R.id.buttonEditarDados)
+        buttonEditarDados.setOnClickListener{
+        if (!nameEdit.isVisible) {
+            nameEdit.visibility = View.VISIBLE
+            textViewEmail.visibility = View.INVISIBLE
+
+            emailEdit.visibility = View.VISIBLE
+            textViewEmail.visibility = View.INVISIBLE
+        }else{
+            nameEdit.visibility = View.INVISIBLE
+            nameTextView.visibility = View.VISIBLE
+            nameTextView.text = nameEdit.text
+
+            emailEdit.visibility = View.INVISIBLE
+            textViewEmail.visibility = View.VISIBLE
+            textViewEmail.text = emailEdit.text
+        }
         }
     }
 }
